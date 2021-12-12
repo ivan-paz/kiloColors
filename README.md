3c626c3f4cea65b9c20aeb0d18f4ca1616eea842
ivan-paz <ivnpaz@gmail.com>
2021-12-12 21:20:29 +0100

    blob detections using scikit-image

---
+++ README.md
@@ -0,0 +1,21 @@
+# Blob detection and color counting
+
+folder countColorBlobs
+contains Images/out00x.jpg
+
+run 
+
+python countBlobs.py 
+
+to detect the blobs and assigns to each one the mean color R, G or B of the pixels contained in that blob
+
+the file results.csv contains:
+
+file, color, number_of_kilobots_with_this_color
+
+ruby results.csv
+
+returns the number of blobs detected in the frame
+
+
+
---
+++ blobs_opencv.py
@@ -0,0 +1,67 @@
+# Standard imports
+import cv2
+import numpy as np
+from matplotlib import pyplot as plt
+
+# Read image
+filename= "out003.jpg"
+img = cv2.imread(filename, 0)
+img_color = cv2.imread(filename, cv2.IMREAD_ANYCOLOR)
+img_c = cv2.resize(img_color,(1080,1920))
+img1 = cv2.resize(img,(1080,1920))
+ret,im = cv2.threshold(img1,120,255,cv2.THRESH_BINARY)
+
+#######################################################
+#######################################################
+
+# Setup SimpleBlobDetector parameters.
+params = cv2.SimpleBlobDetector_Params()
+
+# Change thresholds
+params.minThreshold = 50
+params.maxThreshold = 150
+
+# Filter by Area.
+params.filterByArea = True
+params.minArea = 50
+params.maxArea = 400
+
+# Filter by Circularity
+params.filterByCircularity = True
+params.minCircularity = 0.2
+
+# Filter by Convexity
+params.filterByConvexity = True
+params.minConvexity = 0.1
+
+# Filter by Inertia
+params.filterByInertia = True
+params.minInertiaRatio = 0.01
+
+detector = cv2.SimpleBlobDetector_create(params)
+
+#######################################################
+#######################################################
+
+# Detect blobs.
+keypoints = detector.detect(im)
+
+# Draw detected blobs as red circles.
+# cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS ensures the size of the circle corresponds to the size of blob
+im_with_keypoints = cv2.drawKeypoints(img_c, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+
+# Show keypoints
+cv2.imshow("Keypoints", im_with_keypoints)
+
+#x=[]
+#y=[]
+#area=[]
+#
+#for i in xrange(9):
+#    xx = keypoints[i].pt[0]
+#    yy = keypoints[i].pt[1]
+#    aarea = keypoints[i].size
+#    print("PT.%f -- " %i, "x = %f," %xx, "y = %f," %yy,"area = %f," %aarea, "\n")
+#######################################################
+#######################################################
+cv2.waitKey(0)
