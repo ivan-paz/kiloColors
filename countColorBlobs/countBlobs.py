ff1091f2482048889f0c096226f347386d55eab2
ivan-paz <ivnpaz@gmail.com>
2021-12-17 17:34:40 +0100

    is this a git repo

--- countColorBlobs/countBlobs.py
+++ countColorBlobs/countBlobs.py
@@ -1,7 +1,7 @@
 import numpy as np
 from skimage.io import imread, imshow
 from math import sqrt
-from skimage import data
+from skimage import data ####
 from skimage.feature import blob_dog, blob_log, blob_doh
 from skimage.color import rgb2gray
 import matplotlib.pyplot as plt
