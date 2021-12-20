dcafffa0ef161c866ed906ea3189c024bb39f7a0
ivan-paz <ivnpaz@gmail.com>
2021-12-20 16:36:22 +0100

    kilocounter

---
+++ blobs_Difference_of_Gaussian_Laplacian_of_Gaussian/kiloColorCouter.py
@@ -0,0 +1,29 @@
+import os
+import pandas
+import shutil
+import subprocess
+
+# remove images in the folder
+path = os.chdir('./')
+if os.path.exists('results_difference_of_gaussian.txt'):
+    os.remove('results_difference_of_gaussian.txt')
+
+print('-------------------------------------------')
+print(' ')
+print("file results_difference_of_gaussian.txt removed!")
+print(' ')
+print('-------------------------------------------')
+
+os.system("python count_blobs_extract_mean_color_difference_of_Gaussian.py")
+
+os.system("time python ultimateCounter.py")
+
+if os.path.exists('results_difference_of_gaussian.txt'):
+    os.remove('results_laplace_of_gaussian.txt')
+print('-------------------------------------------')
+
+os.system("time python count_blobs_extract_mean_color_Laplacian_of_Gaussian.py")
+
+os.system("time python countLaplaceOfGaussian.py")
+
+print("Thanks for using kilocounter! Have a nice day!")
